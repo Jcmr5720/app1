26a924b9caa042243ab1ca7736e6ab85604948af
Juan Camilo Muñoz Rodríguez <132085703+Jcmr5720@users.noreply.github.com>
2025-06-23 16:47:25 -0500

    Add missing R imports

--- app/src/main/java/com/example/holamundo/todo/TaskAdapter.java
+++ app/src/main/java/com/example/holamundo/todo/TaskAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.holamundo.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
